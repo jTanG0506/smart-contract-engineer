9ad284d6cb64411d4ea1c3123586ba61f8d99c65
jTanG0506 <mailto.jtang@gmail.com>
2022-09-04 23:38:54 +0100

    feat: DefaultValues

---
+++ DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract DefaultValues {
+    int256 public i; // 0
+    bytes32 public b32; // 0x0000000000000000000000000000000000000000000000000000000000000000
+    address public addr; // 0x0000000000000000000000000000000000000000
+    uint256 public u; // 0
+    bool public b; // false
+}
