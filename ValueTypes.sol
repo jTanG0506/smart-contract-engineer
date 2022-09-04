1806bce929cc3f52d88756da4d3a4946bc0f219f
jTanG0506 <mailto.jtang@gmail.com>
2022-09-04 23:27:09 +0100

    feat: ValueTypes

---
+++ ValueTypes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ValueTypes {
+    bool public b = true;
+    int256 public i = -5;
+    uint256 public u = 123;
+    address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 public b32 =
+        0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
+}
