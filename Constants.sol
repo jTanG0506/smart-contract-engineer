d3dffba72bc8f260e17746548965f3b3a514e5a0
jTanG0506 <mailto.jtang@gmail.com>
2022-09-04 23:40:14 +0100

    feat: Constants

---
+++ Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+    address public constant MY_ADDR =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
